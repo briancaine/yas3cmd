c52c92409fbcab58529098ff9537739fbe2c905f
Brian Caine <brian.d.caine@gmail.com>
2017-04-14 21:38:42 -0400

    Removed OASIS contents of _tags and myocamlbuild.ml

--- s3_test.sh
+++ s3_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p _coverage/ coverage/
 rm -f _coverage/*
 BISECT_FILE=_coverage/bisect ./s3_test.byte $@
 bisect-ppx-report -I src/ -I src/s3/ -I test/ -html coverage/ _coverage/*
