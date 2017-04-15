e099679befd5e3aec743e73728927d8d40ac8568
Brian Caine <brian.d.caine@gmail.com>
2017-04-15 00:42:41 -0400

    Added sudo: required

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,21 +1,19 @@
-# OPAM version to install
-export OPAM_VERSION=1.2.2
+# OCaml version to install
+export OCAML_VERSION=4.03.0
 # OPAM packages needed to build tests
-export OPAM_PACKAGES='ocamlfind ounit core ppx_deriving ppx_fields_conv ppx_sexp_conv cohttp base64 nocrypto'
+export OPAM_PACKAGES='ocamlfind ounit oasis core ppx_deriving ppx_fields_conv ppx_sexp_conv cohttp base64 nocrypto oasis bisect_ppx'
 
 # install ocaml from apt
+#sudo add-apt-repository --yes ppa:avsm/ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml
+sudo apt-get install -qq ocaml ocaml-findlib libgmp-dev aspcud rsync
 
 # install opam
-curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
-pushd /tmp/opam-${OPAM_VERSION}
-./configure
-make
-sudo make install
-opam init
-eval `opam config -env`
-popd
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sudo sh -s /usr/local/bin
+
+opam init -a --compiler=$OCAML_VERSION
+opam switch $OCAML_VERSION
+eval `opam config env`
 
 # install packages from opam
 opam install -q -y ${OPAM_PACKAGES}
@@ -24,4 +22,4 @@ oasis setup
 
 # compile & run tests (here assuming OASIS DevFiles)
 ./configure --enable-tests
-make test
+BISECT_FILE=$PWD/_coverage/bisect BISECT_COVERAGE=YES make test
